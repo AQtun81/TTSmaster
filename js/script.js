19a040601d586ac3758fbc969513ef424359c6e1
AQtun81 <Lysy9990@Gmail.com>
2024-12-12 11:46:25 +0100

    code refactor

--- js/script.js
+++ js/script.js
@@ -1,18 +1,263 @@
 const baseUrl = "https://tkap1.github.io/";
-const surfersContainer = document.getElementById("surfers");
+const waveformsContainer = document.getElementById("waveforms");
 const rowsContainer = document.getElementById("rows");
 const soundnameContainer = document.getElementById("soundname");
 const textInput = document.querySelector("#addbar > input");
 const result = document.querySelector("#commandResult");
 const soundsDatalist = document.querySelector("#sounds");
-const surfers = [];
-const backwards = [];
+
+const waveforms = [];
+var activeWaveform = 0;
+var increment = 0;
+var isFullPlayback = false;
 
 var soundList = [];
-var activeSurfer = 0;
-var mousedown = -1;
-var lastCreatedRegion;
-var isFullPlayback = false;
+
+/* WAVEFORM FUNCTIONS
+--------------------------------------------------------------------------------------------------------------------------------------- */
+
+function GetWaveform(index) {
+  for (const w of waveforms) {
+    if (w.id == index) {
+      return w;
+    }
+  }
+  return null;
+}
+
+function AddWaveform(name = "soulbitch") {
+
+  audioPath = `${baseUrl}sounds/${name}.mp3`
+
+  /* Create Waveform
+  --------------------------------------------------------- */
+  var newWaveformElement = document.createElement('div');
+  var region = WaveSurfer.Regions.create();
+  newWaveformElement.setAttribute("waveform_id", waveforms.length);
+  var newWaveform = WaveSurfer.create({
+    container: newWaveformElement,
+    waveColor: "#1b3857",
+    progressColor: "#3975b5",
+    plugins: [region],
+    height: 100,
+    url: audioPath
+  });
+  waveformsContainer.appendChild(newWaveformElement);
+
+  // waveform - play on click
+  newWaveform.on('click', () => {
+    if (newWaveform.media.paused ||
+      newWaveform.plugins[0].regions.length < 1) {
+      newWaveform.play();
+    } else {
+      newWaveform.pause();
+    }
+  })
+
+  // waveform - play in sequence
+  newWaveform.media.addEventListener("ended", (e) => {
+    FullPlaybackNext();
+  })
+
+  // rehion settings and events
+  region.enableDragSelection({
+    color: 'rgba(255, 0, 0, 0.1)',
+  })
+
+  region.on('region-created', (r) => {
+    if (region.regions.length > 1) {
+      region.regions[0].remove();
+    }
+    UpdateResult();
+  });
+
+  region.on('region-out', (r) => {
+    if (isFullPlayback) {
+      if (newWaveform.media.currentTime + 0.02 < r.end) {
+        return;
+      }
+      newWaveform.media.pause();
+      FullPlaybackNext();
+      return;
+    }
+    r.play();
+  });
+
+  region.on('region-updated', (r) => {
+    UpdateResult();
+  });
+
+  /* Create Row
+  --------------------------------------------------------- */
+  var row = document.createElement('div');
+  row.setAttribute("waveform_id", increment);
+
+  // backwards toggle button
+  var toggleBackwardsButton = document.createElement('button');
+  toggleBackwardsButton.classList.add("toggleBackwards");
+  toggleBackwardsButton.addEventListener("click", (e) => {
+    var state = toggleBackwardsButton.classList.contains("active");
+    if (state) {
+      toggleBackwardsButton.classList.remove("active");
+    } else {
+      toggleBackwardsButton.classList.add("active");
+    }
+    GetWaveform(toggleBackwardsButton.parentNode.attributes.waveform_id.value).backwards = !state;
+    UpdateResult();
+    toggleBackwardsButton.blur();
+  });
+  row.appendChild(toggleBackwardsButton);
+
+  // sound name label
+  var label = document.createElement('h5');
+  label.innerText = name;
+  label.addEventListener("click", (e) => {
+    SetActiveWaveform(row.getAttribute("waveform_id"));
+  })
+  row.appendChild(label);
+
+  // delete sound button
+  var deleteButton = document.createElement('button');
+  deleteButton.classList.add("delete");
+  deleteButton.addEventListener("click", (e) => {
+    DeleteWaveform(row.getAttribute("waveform_id"));
+  });
+  row.appendChild(deleteButton);
+
+  rowsContainer.appendChild(row);
+
+  // append to array
+  waveforms.push({
+    'name': name,
+    'waveformElement': newWaveformElement,
+    'wavesurfer': newWaveform,
+    'region': region,
+    'row': row,
+    'backwards': false,
+    'id': increment,
+  });
+
+  SetActiveWaveform(increment);
+  increment++;
+  UpdateResult();
+}
+
+function SetActiveWaveform(index) {
+  if (activeWaveform == index) return;
+  waveform = GetWaveform(index);
+  if (!waveform) return;
+
+  activeWaveform = index;
+
+  for (const w of waveforms) {
+    w.waveformElement.style.opacity = "0";
+    w.waveformElement.style.zIndex = "0";
+    w.row.classList.remove("active");
+  }
+  waveform.waveformElement.style.opacity = "1";
+  waveform.waveformElement.style.zIndex = "100";
+  waveform.row.classList.add("active");
+  soundnameContainer.innerText = waveform.name;
+}
+
+function DeleteWaveform(index) {
+  var waveform = GetWaveform(index);
+  if (!waveform) return;
+
+  waveform.waveformElement.remove();
+  waveform.row.remove();
+
+  const position = waveforms.indexOf(waveform);
+  waveforms.splice(position, 1);
+
+  UpdateResult();
+  if (waveforms.length < 1) return;
+  SetActiveWaveform(waveforms[0].id);
+}
+
+/* SEQUENCIAL PLAYBACK
+--------------------------------------------------------------------------------------------------------------------------------------- */
+
+function PlaySoundsInSequence() {
+  if (rowsContainer.children.length < 1) return;
+  if (isFullPlayback) {
+    isFullPlayback = false;
+    GetWaveform(activeWaveform).wavesurfer.media.pause();
+    return;
+  }
+  SetActiveWaveform(waveforms[0].id);
+  if (waveforms[0].region.regions.length > 0) {
+    waveforms[0].region.regions[0].play();
+  } else {
+    waveforms[0].wavesurfer.media.currentTime = 0;
+    waveforms[0].wavesurfer.media.play();
+  }
+  isFullPlayback = true;
+}
+
+function FullPlaybackNext() {
+  if (!isFullPlayback) return;
+  if (rowsContainer.lastChild.attributes.waveform_id.value == activeWaveform) {
+    isFullPlayback = false;
+    return;
+  }
+
+  var active = GetWaveform(activeWaveform);
+  index = waveforms.indexOf(active) + 1;
+  index %= waveforms.length;
+  SetActiveWaveform(waveforms[index].id);
+
+  active = GetWaveform(activeWaveform);
+
+  if (active.region.regions.length > 0) {
+    active.region.regions[0].play();
+  } else {
+    active.wavesurfer.media.currentTime = 0;
+    active.wavesurfer.media.play();
+  }
+}
+
+/* GENERATED TTS COMMAND
+--------------------------------------------------------------------------------------------------------------------------------------- */
+
+function UpdateResult() {
+  var state_backwards = false;
+  var text = "!tts";
+  for (let i = 0; i < waveforms.length; i++) {
+    const w = waveforms[i];
+    if (w.backwards != state_backwards) {
+      text += ` -b`;
+      state_backwards = !state_backwards;
+    }
+    if (w.region.regions.length > 0) {
+      var region = w.region.regions[0];
+      var start = Math.round(region.start * 1000);
+      var end = Math.round(region.end * 1000);
+      text += ` -ck${start}:${end}`;
+    }
+    text += ` -${w.wavesurfer.options.url.split('/').at(-1).split('.')[0]}`;
+  }
+  result.innerText = text;
+}
+
+async function CopyCommandToClipboard() {
+  navigator.clipboard.writeText(result.innerText);
+  var toast = document.querySelector("#commandResultToast");
+  toast?.classList.add("visible");
+  await new Promise(r => setTimeout(r, 2000));
+  toast?.classList.remove("visible");
+}
+
+/* UI FUNCTIONS
+--------------------------------------------------------------------------------------------------------------------------------------- */
+
+function AddSoundTextInput() {
+  if (!soundList.includes(textInput.value)) return;
+  AddWaveform(textInput.value);
+}
+
+/* SOUND LIST
+--------------------------------------------------------------------------------------------------------------------------------------- */
 
 async function UpdateSoundList() {
   const response = await fetch(`${baseUrl}list.txt`);
@@ -40,279 +285,29 @@ async function UpdateSoundList() {
   }
 }
 
-function SetActiveSurfer(newActive) {
-  if (activeSurfer == newActive) return;
-  if (newActive >= surfers.length) return;
-  if (!surfers[newActive]) return;
-  activeSurfer = parseInt(newActive);
+UpdateSoundList();
 
-  for (const e of surfersContainer.children) {
-    e.style.opacity = "0";
-    e.style.zIndex = "0";
-  }
-  
-  var activeDiv = document.querySelector(`#surfers div[surfer_id="${activeSurfer}"]`);
-  activeDiv.style.opacity = "1";
-  activeDiv.style.zIndex = "100";
-
-  soundnameContainer.innerText = surfers[activeSurfer].options.url.split('/').at(-1).split('.')[0];
-
-  for (const e of rowsContainer.children) e.classList.remove("active");
-
-  activeDiv = document.querySelector(`#rows div[surfer_id="${activeSurfer}"]`);
-
-  if (!activeDiv) return;
-  activeDiv.classList.add("active");
-}
-
-function SetFirstSurfer() {
-  if (rowsContainer.children < 1) return;
-  SetActiveSurfer(rowsContainer.children[0]?.attributes.surfer_id.value);
-}
-
-function AddSurfer(audioPath = `${baseUrl}sounds/soulbitch.mp3`) {
-  var newSurfer = document.createElement('div');
-  newSurfer.setAttribute("surfer_id", surfers.length);
-  surfers.push(WaveSurfer.create({
-    container: newSurfer,
-    waveColor: "#1b3857",
-    progressColor: "#3975b5",
-    plugins: [WaveSurfer.Regions.create()],
-    height: 100,
-    url: audioPath
-  }));
-  backwards.push(false);
-  surfersContainer.appendChild(newSurfer);
-
-  // play on click
-  surfers[surfers.length - 1].on('click', () => {
-    if (surfers[activeSurfer].media.paused ||
-        surfers[activeSurfer].plugins[0].regions.length < 1) {
-      if (Date.now() - lastCreatedRegion < 100) return;
-      surfers[activeSurfer].play();
-    } else {
-      surfers[activeSurfer].pause();
-    }
-  })
-
-  // play sequence
-  surfers[surfers.length - 1].media.addEventListener("ended", (e) => {
-    FullPlaybackNext();
-  })
-
-  // create region press
-  newSurfer.addEventListener("mousedown", (e) => {
-    mousedown = e.clientX;
-  })
-
-  // create region release
-  newSurfer.addEventListener("mouseup", (e) => {
-    if (mousedown == -1) return;
-    if (Math.abs(mousedown - e.clientX) < 10) return;
-    var width = 1.0 / surfers[activeSurfer].renderer.canvasWrapper.getBoundingClientRect().width;
-    var from = mousedown * width * surfers[activeSurfer].media.duration;
-    var to = e.clientX * width * surfers[activeSurfer].media.duration;
-    AddRegion(from, to);
-    mousedown = -1;
-    e.preventDefault();
-  })
-}
-
-function DeleteSurfer(surfer_id) {
-  var surferElement = document.querySelector(`#surfers [surfer_id="${surfer_id}"]`);
-  var rowElement = document.querySelector(`#rows [surfer_id="${surfer_id}"]`);
-  surfers[surfer_id] = null;
-  rowElement.remove();
-  surferElement.remove();
-  if (surfer_id == activeSurfer) SetFirstSurfer();
-  UpdateResult();
-}
-
-function AddRegion(start = 0.0, end = 0.1) {
-
-  if (start > end) {
-    var temp = start;
-    start = end;
-    end = temp;
-  }
-
-  if (surfers[activeSurfer].options.plugins[0].regions.length > 0) return;
-  surfers[activeSurfer].options.plugins[0].addRegion({
-    start: start,
-    end: end,
-    content: "Cut",
-    color: "#fff4",
-    drag: true,
-    resize: true,
-  });
-  lastCreatedRegion = Date.now();
-
-  surfers[activeSurfer].options.plugins[0].on('region-out', (region) => {
-    if (isFullPlayback) {
-      if (surfers[activeSurfer].media.currentTime + 0.02 < region.end) {
-        return;
-      }
-      surfers[activeSurfer].media.pause();
-      FullPlaybackNext();
-      return;
-    }
-    region.play();
-  });
-
-  surfers[activeSurfer].options.plugins[0].on('region-updated', (region) => {
-    var start = Math.round(region.start * 1000);
-    var end = Math.round(region.end * 1000);
-    region.content.innerText = `Cut ${start} - ${end}`;
-    surfers[activeSurfer].media.currentTime = region.start;
-    UpdateResult();
-  });
-
-  var region = surfers[activeSurfer].options.plugins[0].regions[0];
-  var start = Math.round(region.start * 1000);
-  var end = Math.round(region.end * 1000);
-  region.content.innerText = `Cut ${start} - ${end}`;
-
-  UpdateResult();
-}
-
-function AddRow(name) {
-  var row = document.createElement('div');
-  row.setAttribute("surfer_id", surfers.length - 1);
-
-  var toggleBackwardsButton = document.createElement('button');
-  toggleBackwardsButton.classList.add("toggleBackwards");
-  toggleBackwardsButton.addEventListener("click", (e) => {
-    var state = e.target.classList.contains("active");
-    if (state) {
-      e.target.classList.remove("active");
-    } else {
-      e.target.classList.add("active");
-    }
-    backwards[e.target.parentNode.attributes.surfer_id.value] = !state;
-    UpdateResult();
-    e.target.blur();
-  });
-  row.appendChild(toggleBackwardsButton);
-
-  var label = document.createElement('h5');
-  label.innerText = name;
-  label.addEventListener("click", (e) => {
-    SetActiveSurfer(row.getAttribute("surfer_id"));
-  })
-  row.appendChild(label);
-
-  var deleteButton = document.createElement('button');
-  deleteButton.classList.add("delete");
-  deleteButton.addEventListener("click", (e) => {
-    DeleteSurfer(row.getAttribute("surfer_id"));
-  });
-  row.appendChild(deleteButton);
-
-  rowsContainer.appendChild(row);
-}
-
-function AddSound(name) {
-  AddSurfer(`${baseUrl}/sounds/${name}.mp3`);
-  AddRow(name);
-  SetActiveSurfer(surfers.length - 1);
-
-  UpdateResult();
-}
-
-function UpdateResult() {
-  var state_backwards = false;
-  var text = "!tts";
-  for (let i = 0; i < surfers.length; i++) {
-    const s = surfers[i];
-    if (!s) continue;
-    if (backwards[i] != state_backwards) {
-      text += ` -b`;
-      state_backwards = !state_backwards;
-    }
-    if (s.plugins[0].regions.length > 0) {
-      var region = s.plugins[0].regions[0];
-      var start = Math.round(region.start * 1000);
-      var end = Math.round(region.end * 1000);
-      text += ` -ck${start}:${end}`;
-    }
-    text += ` -${s.options.url.split('/').at(-1).split('.')[0]}`;
-  }
-  result.innerText = text;
-}
-
-async function CopyCommandToClipboard() {
-  navigator.clipboard.writeText(result.innerText);
-  var toast = document.querySelector("#commandResultToast");
-  toast?.classList.add("visible");
-  await new Promise(r => setTimeout(r, 2000));
-  toast?.classList.remove("visible");
-}
-
-function PlaySoundsInSequence() {
-  if (rowsContainer.children.length < 1) return;
-  if (isFullPlayback) {
-    isFullPlayback = false;
-    surfers[activeSurfer].media.pause();
-    return;
-  }
-  SetFirstSurfer();
-  if (surfers[activeSurfer].plugins[0].regions.length > 0) {
-    surfers[activeSurfer].plugins[0].regions[0].play();
-  } else {
-    surfers[activeSurfer].media.currentTime = 0;
-    surfers[activeSurfer].media.play();
-  }
-  isFullPlayback = true;
-}
-
-function AddSoundTextInput() {
-  if (!soundList.includes(textInput.value)) return;
-  AddSound(textInput.value);
-}
+/* EVENTS
+--------------------------------------------------------------------------------------------------------------------------------------- */
 
 textInput.addEventListener("keydown", (e) => {
+  if (e.code == "Space") e.preventDefault();
   if (e.code != "Enter") return;
   AddSoundTextInput();
 })
 
 window.addEventListener("wheel", (e) => {
-  if (surfers.length < 2) return;
-
-  var newActive = activeSurfer;
+  if (waveforms.length < 2) return;
   var step = e.deltaY >= 0 ? 1 : -1;
-  do {
-    newActive += step;
-    newActive %= surfers.length;
-    if (newActive < 0) newActive += surfers.length;
-  } while (!surfers[newActive]);
-  SetActiveSurfer(newActive);
+  var active = GetWaveform(activeWaveform);
+  index = waveforms.indexOf(active);
+  index += step;
+  index %= waveforms.length;
+  if (index < 0) index += waveforms.length;
+  SetActiveWaveform(waveforms[index].id);
 });
 
 window.addEventListener("keydown", (e) => {
   if (e.code != "Space") return;
   PlaySoundsInSequence();
 })
-
-function FullPlaybackNext() {
-  if (!isFullPlayback) return;
-  if (rowsContainer.lastChild.attributes.surfer_id.value == activeSurfer) {
-    isFullPlayback = false;
-    return;
-  }
-
-  var newSurfer = activeSurfer;
-  do {
-    newSurfer += 1;
-  } while (!surfers[newSurfer]);
-  SetActiveSurfer(newSurfer);
-
-  if (surfers[activeSurfer].plugins[0].regions.length > 0) {
-    surfers[activeSurfer].plugins[0].regions[0].play();
-  } else {
-    surfers[activeSurfer].media.currentTime = 0;
-    surfers[activeSurfer].media.play();
-  }
-}
-
-
-UpdateSoundList();
